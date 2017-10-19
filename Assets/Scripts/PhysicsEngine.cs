4d7e4dfe8dac44155682603fd1bc91b932f3b440
JOHAN-PC3\Johan <johan.lambot@gmail.com>
2017-10-19 20:44:57 +0200

    Newton's 1st Law

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+
+    /// <summary>
+    /// average velocity this fixedupdate()
+    /// </summary>
+    public Vector3 m_v;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 deltaS = m_v * Time.deltaTime;
+        this.transform.position += deltaS;
+    }
+}
