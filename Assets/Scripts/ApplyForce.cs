a7a65ad036a4f21a13e5dd43bf98c867f0b78f2c
JOHAN-PC3\Johan <johan.lambot@gmail.com>
2017-10-21 12:05:27 +0200

    Physics Engine Architecture

---
+++ Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class ApplyForce : MonoBehaviour {
+    
+    public Vector3 m_forceVector;
+
+    private PhysicsEngine m_physicsEngineComp;
+
+    private void Start()
+    {
+        m_physicsEngineComp = this.GetComponent<PhysicsEngine>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_physicsEngineComp.AddForce(m_forceVector);
+    }
+}
--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -13,35 +13,85 @@ public class PhysicsEngine : MonoBehaviour {
     public float m_mass = 1.0f;
 
 
-    public List<Vector3> m_forceVectorList = new List<Vector3>();
+    private List<Vector3> m_forceVectorList = new List<Vector3>();
+
+
+    /// <summary>
+    /// TRAIL
+    /// </summary>
+    public bool showTrails = true;
+
+    private LineRenderer lineRenderer;
+    private int numberOfForces;
 
     // Use this for initialization
-    void Start ()
-    { 
+    private void Start()
+    {
+        SetupTrails();
     }
 
     private void FixedUpdate()
     {
-        AddForces();
-        UpdateVelocity();
-
-        //update position
-        this.transform.position += m_velovityVector * Time.deltaTime;
+        RenderTrails();  
+        UpdatePosition();
     }
 
-    private void AddForces()
-    {
-        m_netForceVector = Vector3.zero;
 
-        for(int i = 0; i < m_forceVectorList.Count; ++i)
+    public void AddForce(Vector3 forceToAdd)
+    {
+        m_forceVectorList.Add(forceToAdd);
+    }
+
+    public List<Vector3> GetForceVectorList()
+    {
+        return m_forceVectorList;
+    }
+
+    private void SetupTrails()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.SetColors(Color.yellow, Color.yellow);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.useWorldSpace = false;
+    }
+
+    private void RenderTrails()
+    {
+        if (showTrails)
+        {
+            lineRenderer.enabled = true;
+            numberOfForces = m_forceVectorList.Count;
+            lineRenderer.SetVertexCount(numberOfForces * 2);
+            int i = 0;
+            foreach (Vector3 forceVector in m_forceVectorList)
+            {
+                lineRenderer.SetPosition(i, Vector3.zero);
+                //lineRenderer.SetPosition(i + 1, forceVector);
+                lineRenderer.SetPosition(i + 1, -forceVector);
+                i = i + 2;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }    
+
+    private void UpdatePosition()
+    {
+        //Sum forces and clear list
+        m_netForceVector = Vector3.zero;
+        for (int i = 0; i < m_forceVectorList.Count; ++i)
         {
             m_netForceVector += m_forceVectorList[i];
         }
-    }
+        
+        m_forceVectorList.Clear();
 
-    private void UpdateVelocity()
-    {
+        //Update position with net force
         Vector3 accelerationVector = m_netForceVector / m_mass;
         m_velovityVector += accelerationVector * Time.deltaTime;
+        this.transform.position += m_velovityVector * Time.deltaTime;
     }
 }
