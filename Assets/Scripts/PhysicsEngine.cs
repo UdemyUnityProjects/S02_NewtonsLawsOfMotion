dece9e57747cd6096a7cd7e672c9b1680ded8bfa
JOHAN-PC3\Johan <johan.lambot@gmail.com>
2017-10-21 18:51:38 +0200

    Script Execution Order

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -15,9 +15,9 @@ public class PhysicsEngine : MonoBehaviour {
 
     private List<Vector3> m_forceVectorList = new List<Vector3>();
 
-    private PhysicsEngine[] m_physicsEngineArray;
+    
 
-    private const float BIG_G = 6.673e-11f; // m^3 s^-2 kg^-1 ]
+    
 
 
     /// <summary>
@@ -33,14 +33,12 @@ public class PhysicsEngine : MonoBehaviour {
     {
         SetupTrails();
 
-        m_physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine>();
+        
     }
 
     private void FixedUpdate()
     {
-       
-        CalculateGravity();
-        RenderTrails();
+        RenderTrails();     
         UpdatePosition();
     }
 
@@ -57,46 +55,7 @@ public class PhysicsEngine : MonoBehaviour {
     }
 
     
-    private void CalculateGravity()
-    {
-        for(int i = 0; i < this.m_physicsEngineArray.Length; ++i)
-        {
-            for(int j = 0;  j < this.m_physicsEngineArray.Length; ++j)
-            {
-                if (i == j)
-                    continue;
 
-                PhysicsEngine physicsEngineA = this.m_physicsEngineArray[i];
-
-                if (physicsEngineA == this)
-                    continue;
-
-                PhysicsEngine physicsEngineB = this.m_physicsEngineArray[j];
-
-                
-
-                Debug.Log("Calculating gravitational force exerted on " + physicsEngineA.name +
-                        " due to the gravity of " + physicsEngineB.name);
-
-
-                Vector3 gravityFeltVector = Vector3.zero;
-
-                //F = G * ((massA * massB) / distance * distance )
-                //G = 6.7f
-                //https://en.wikipedia.org/wiki/Gravitational_constant
-
-                float mulMass = physicsEngineA.m_mass * physicsEngineB.m_mass;
-                Vector3 forceDirection = physicsEngineB.transform.position - physicsEngineA.transform.position;
-                float sqrDistance = (forceDirection).sqrMagnitude;
-
-                float gravityMagnitude = (BIG_G * (mulMass / sqrDistance));
-
-                gravityFeltVector = forceDirection.normalized * gravityMagnitude;
-
-                physicsEngineA.AddForce(gravityFeltVector);
-            }
-        }
-    }
 
     private void UpdatePosition()
     {
