280048e2df3c2babae7914a3befd940b06c41193
JOHAN-PC3\Johan <johan.lambot@gmail.com>
2017-10-21 16:10:30 +0200

    Rocket Science 101

--- Assets/Scripts/RocketEngine.cs
+++ Assets/Scripts/RocketEngine.cs
@@ -8,19 +8,62 @@ public class RocketEngine : MonoBehaviour {
 
     public float m_fuelMass;                        //[kg]
     public float m_maxThrust;                       //kN [kg m s^-2]
+
+    [Range(0, 1.0f)]
     public float m_thrustPercent;                   //[none]
 
     public Vector3 m_thrustUnitVector;              //[none]
+
+    private float m_currentThrust;                  //N
 
     private PhysicsEngine m_physicsEngineComp;
 
     private void Start()
     {
         m_physicsEngineComp = this.GetComponent<PhysicsEngine>();
+        m_physicsEngineComp.m_mass += m_fuelMass;
     }
 
     private void FixedUpdate()
     {
-        m_physicsEngineComp.AddForce(m_thrustUnitVector);
+        if(m_fuelMass > FuelThisUpdate())
+        {
+            //reduce fuel mass
+            m_fuelMass -= FuelThisUpdate();
+            m_physicsEngineComp.m_mass -= FuelThisUpdate();
+
+            ExertForce();
+        }
+        else
+        {
+            Debug.LogWarning("Out of rocket fuel !");
+        }        
+    }
+
+    private float FuelThisUpdate()
+    {
+        float exhaustMassFlow;
+        float effectiveExhaustVelocity;
+
+        //[m s^-1] liquid H O
+        effectiveExhaustVelocity = 4462.0f;
+
+        //thrust = massFlow * exhaustVelocity
+        //massFlow = thrust / exhaustVelocity
+        exhaustMassFlow = m_currentThrust / effectiveExhaustVelocity;
+
+        //[kg]
+        return exhaustMassFlow * Time.deltaTime;
+    }
+
+    private void ExertForce()
+    {
+        //*1000.0f to convert kN to N (Newton)
+        m_currentThrust = m_thrustPercent * (m_maxThrust * 1000.0f);
+
+        //N
+        Vector3 thrustVector = m_thrustUnitVector.normalized * m_currentThrust;
+
+        m_physicsEngineComp.AddForce(thrustVector);
     }
 }
